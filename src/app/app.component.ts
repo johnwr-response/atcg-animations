e320b7f5254ccc33afd9182bb2e45978390af2fd
johnwr-response <52628290+johnwr-response@users.noreply.github.com>
2020-05-14 22:33:58 +0200

    The "void" State

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,7 +41,26 @@ import {animate, state, style, transition, trigger} from "@angular/animations";
         })),
         animate(500)
       ])
-    ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ]),
+    ]),
   ]
 })
 export class AppComponent {
@@ -56,14 +75,13 @@ export class AppComponent {
 
   onShrink() {
     this.wildState = 'shrunken';
-
   }
 
   onAdd(item) {
     this.list.push(item);
   }
 
-  onDelete() {
-
+  onDelete(item: string) {
+    this.list.splice(this.list.indexOf(item), 1);
   }
 }
